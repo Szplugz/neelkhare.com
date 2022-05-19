9ca3533cb8a27956dff6e23d5cf6683b9a32274b
Neel <69532488+Szplugz@users.noreply.github.com>
2022-05-19 17:37:23 -0500

    add webfont + basic split layout

---
+++ src/Landing.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div className="left-box fixed top-0 h-screen w-1/2 bg-black">
+      <div className="menu text-white font-forma">
+        <ul>
+          <li>Neel Khare</li>
+          <li>Bits</li>
+          <li>Intrigue</li>
+          <li>Inspiration</li>
+          <li>Projects</li>
+          <li>Resumé</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
+  mode: "jit",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        forma: ["forma-djr-micro", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
