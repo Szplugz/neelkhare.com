5d7c7b34ba9c7c3766a1759e68ba685a84fc08b1
Neel <69532488+Szplugz@users.noreply.github.com>
2022-06-26 15:11:57 -0500

    imporovements

--- src/About.jsx
+++ src/About.jsx
@@ -1,81 +1,113 @@
 import React from "react";
 import Navbar from "./Navbar";
 import biopic from "./utils/biopic.jpg";
+import { Helmet } from "react-helmet";
+import "./About.css";
 
 const About = () => {
   return (
     <div className="about-page bg-almostBlack flex flex-row justify-center">
+      <Helmet>
+        <title>About | Neel Khare</title>
+      </Helmet>
       <Navbar />
       <div className="about-bio flex flex-col w-[30%] my-[10vh] mx-[5vw]">
-        <div className="about-picture-container flex flex-row w-full justify-start items-start h-[35vh] my-2vh">
+        {/* <div className="about-picture-container flex flex-row w-full justify-start items-start h-[35vh] my-2vh">
           <img className="picture rounded w-auto h-[90%]" src={biopic}></img>
-        </div>
+        </div> */}
         <div className="about-bio-content flex-col w-full text-eggshell font-tablet font-[400] leading-snug">
           <p className="p1 mb-[3vh]">
             Hi, I'm Neel! I spend most of the year studying computer science and
             writing code at the University of Illinois, Urbana-Champaign, where
-            I am a junior. Two of my biggest areas of interest related to
-            programming are transporation and finance, so in the near future I
-            see myself building software that solves problems in these domains.
-            At the moment, I'm quite interested in full-stack development and
-            product design. I find myself using C++ and Javascript the most, so
-            you can be assured that I have a high pain threshold.
+            I am a junior. What matters to me the most is consistent personal
+            growth and sharing my knowledge with the world in the hope of
+            lifting others up. At the moment, I'm quite interested in full-stack
+            development and product design, and I am always looking to involve
+            myself in projects that improve people’s quality of life. I find
+            myself using C++ and Javascript the most (so you can be assured that
+            I have a high pain threshold).
           </p>
+          <h1 className="h1 mb-[3vh] text-lightpink font-[400]">
+            What keeps me busy these days:
+          </h1>
+          <li className="bullet ml-[4vh] mb-[1vh]">
+            <span className="p1">
+              At college, I am a software developer at{" "}
+              <a
+                className="link text-lightpink"
+                href="https://www.quantillinois.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Quant
+              </a>
+              , and co-founder and President at August Productions, where we
+              make films and documentaries.
+            </span>
+          </li>
+          <li className="bullet ml-[4vh] mb-[1vh]">
+            <span className="p2">
+              I build and maintain side projects that mostly consist of websites
+              and handy web-applications.
+            </span>
+          </li>
+          <li className="bullet ml-[4vh] mb-[3vh]">
+            <span className="p2">
+              I'm a full-stack engineering intern at{" "}
+              <a
+                className="link text-lightpink"
+                href="https://www.mediaocean.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Mediaocean
+              </a>
+              .
+            </span>
+          </li>
           <p className="p1 mb-[3vh]">
-            At college, I am a software developer at{" "}
-            <a
-              className="link text-lightpink"
-              href="https://www.quantillinois.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Quant
-            </a>
-            , and co-founder and President at August Productions, where we make
-            films and documentaries. I am also involved with the Alpha Delta Phi
-            fraternity, which provides a nice getaway from my busy schedule.
-            During my (rare) free time, I like to play soccer, take pictures,
-            read books, make videos, read articles, form opinions, express
-            opinions, go to the gym, take a walk, and grab dinner with friends
-            (one at a time). I would like to learn how to relax.
+            {" "}
+            In my free time, I like roaming around Chicago, exploring cafés,
+            watching trains, taking pictures, and reading. I love learning about
+            human behavior, transportation, and startups, among many other
+            things.
           </p>
           <p className="p1 mb-[3vh]">
             I love connecting with and meeting new people, so please feel free
             to reach out to me through any of the social links that you see on
-            the left. I would <em>especially</em> love to hear from you if:
+            the left! I would <em>especially</em> love to hear from you if:
             <div className="list mt-[3vh]">
-              <p className="about-cat-1 ml-[5%]">
+              <p className="about-cat-1 ml-[5%] mb-[1vh]">
                 {" "}
                 <span className="1 text-lightpink font-tablet">1.&nbsp;</span>
                 You are a recruiter looking for a software engineering intern.
               </p>
-              <p className="about-cat-2 ml-[5%]">
+              <p className="about-cat-2 ml-[5%] mb-[1vh]">
                 {" "}
                 <span className="2 text-lightpink font-tablet">2.&nbsp;</span>
                 You are a developer/filmmaker/photographer looking to
                 collaborate on a project.
               </p>
-              <p className="about-cat-3 ml-[5%]">
+              <p className="about-cat-3 ml-[5%] mb-[1vh]">
                 {" "}
                 <span className="3 text-lightpink font-tablet">3.&nbsp;</span>
-                If you would like to talk (or argue) with me about one of my
+                If you would like to talk to (or argue with) me about one of my
                 many{" "}
                 <span className="about-interests-link text-lightpink font-tablet">
                   interests
                 </span>{" "}
                 .
               </p>
-              <p className="about-cat-4 ml-[5%]">
-                {" "}
-                <span className="4 text-lightpink font-tablet">4.&nbsp;</span>
-                If you have the answer to why all of the highways and roads on
-                the outskirts of Chicago are always under construction >:(
-              </p>
             </div>
           </p>
         </div>
       </div>
-      <div className="about-values flex w-[30%] mt-[10vh]"></div>
+      <div className="about-pictures flex flex-col items-center w-[30%] mt-[10vh]">
+        <img
+          className="picture rounded w-auto h-[50vh] mb-[1vh]"
+          src={biopic}
+        ></img>
+      </div>
     </div>
   );
 };
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Navbar.css";
+import plus from "./utils/add.svg";
+import x from "./utils/x.svg";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="navbar-box flex flex-col justify-between h-[85vh] align-top w-[15%] min-w-480 mt-[10vh] mb-[10vh]">
-      <div className="navbar-menu flex flex-col h-[35%] overflow-y-auto">
+      <div className="navbar-menu flex flex-col h-[35%]">
         <li className="navbar-neel text-lightpink font-forma relative flex items-center font-[300] text-[3vh] hover:text-eggshell transition duration-500 ease-in-out leading-none">
           Neel Khare
-        </li>
-        <li className="navbar-bits text-eggshell font-forma relative flex items-center font-[300] text-[3vh] leading-none">
-          <NavbarItem text={"Bits"} />
         </li>
         <li className="navbar-intrigue text-eggshell font-forma relative flex items-center font-[300] text-[3vh] leading-none">
           <NavbarItem text={"Intrigue"} />
@@ -40,8 +42,16 @@ const Navbar = () => {
         <li className="navbar-websites text-eggshell font-forma relative flex items-center font-[300] text-[3vh] leading-none">
           <NavbarItem text={"Websites"} />
         </li>
+        <li className="navbar-bits text-eggshell font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+          <NavbarItem text={"Tapas"} />
+        </li>
       </div>
       <div className="navbar-external flex flex-col justify-start align-top">
+        <li className="navbar-email text-eggshell font-forma relative flex items-center font-[300] text-[3vh] leading-none hover:text-blue transition duration-500 ease-in-out">
+          <a href="mailto:neelk2@illinois.edu" target="_blank" rel="noreferrer">
+            <NavbarItem text={"email"} />
+          </a>
+        </li>
         <li className="navbar-github text-eggshell font-forma relative flex items-center font-[300] text-[3vh] leading-none hover:text-githubGrey transition duration-500 ease-in-out">
           <a href="https://github.com/Szplugz" target="_blank" rel="noreferrer">
             <NavbarItem text={"github"} />
@@ -74,6 +84,61 @@ const Navbar = () => {
             <NavbarItem text={"youtube"} />
           </a>
         </li>
+      </div>
+      <div className="navbar-mobile">
+        {toggleMenu ? (
+          <img
+            className="x-button h-[6vh] w-auto"
+            src={x}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <img
+            className="plus-button h-[6vh] w-auto"
+            src={plus}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="navbar-menu flex flex-col h-[35%] overflow-y-auto">
+            <li className="navbar-neel text-lightpink font-forma relative flex items-center font-[300] text-[3vh] hover:text-eggshell transition duration-500 ease-in-out leading-none">
+              Neel Khare
+            </li>
+            <li className="navbar-bits text-eggshell font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+              <NavbarItem text={"Bits"} />
+            </li>
+            <li className="navbar-intrigue text-eggshell font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+              <NavbarItem text={"Intrigue"} />
+            </li>
+            <li className="navbar-lyrics text-eggshell font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+              <NavbarItem text={"Lyrics"} />
+            </li>
+            <li className="navbar-people text-eggshell font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+              <NavbarItem text={"People"} />
+            </li>
+            <li className="navbar-photography text-eggshell font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+              <NavbarItem text={"Photography"} />
+            </li>
+            <li className="navbar-projects text-eggshell font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+              <NavbarItem text={"Projects"} />
+            </li>
+            <li className="navbar-quotes text-eggshell font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+              <NavbarItem text={"Quotes"} />
+            </li>
+            <li className="navbar-item">
+              <a
+                href="https://drive.google.com/file/d/1Opg8P9ej8cd1tPoKEsSkcYo7F4J3Uz63/view?usp=sharing"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <NavbarItem text={"Resumé"} />
+              </a>
+            </li>
+            <li className="navbar-websites text-eggshell font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+              <NavbarItem text={"Websites"} />
+            </li>
+          </div>
+        )}
       </div>
     </div>
   );
