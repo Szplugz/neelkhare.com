0f37c0fc417b9a9df5a1cb9675203364b806d1cf
Neel <69532488+Szplugz@users.noreply.github.com>
2022-06-27 12:18:44 -0500

    add missed folders

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,149 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import plus from "../utils/add.svg";
+import x from "../utils/x.svg";
+import biopic from "../utils/biopic.jpg";
+import fallpic from "../utils/fallrl2square.jpg";
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className="navbar-box flex flex-col fixed left-[15vw] justify-between h-[85vh] align-top w-[15%] min-w-480 my-[10vh] mr-[7.5vw]">
+      <div className="navbar-menu flex flex-col">
+        <li className="navbar-intrigue text-almostBlack font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+          <NavbarItem text={"Intrigue"} />
+        </li>
+        <li className="navbar-lyrics text-almostBlack font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+          <NavbarItem text={"Lyrics"} />
+        </li>
+        <li className="navbar-people text-almostBlack font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+          <NavbarItem text={"People"} />
+        </li>
+        <li className="navbar-photography text-almostBlack font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+          <NavbarItem text={"Photography"} />
+        </li>
+        <li className="navbar-projects text-almostBlack font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+          <NavbarItem text={"Projects"} />
+        </li>
+        <li className="navbar-quotes text-almostBlack font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+          <NavbarItem text={"Quotes"} />
+        </li>
+        <li className="navbar-item">
+          <a
+            href="https://drive.google.com/file/d/1Opg8P9ej8cd1tPoKEsSkcYo7F4J3Uz63/view?usp=sharing"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <NavbarItem text={"Resumé"} />
+          </a>
+        </li>
+        <li className="navbar-websites text-almostBlack font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+          <NavbarItem text={"Websites"} />
+        </li>
+        <li className="navbar-bits text-almostBlack font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+          <NavbarItem text={"Tapas"} />
+        </li>
+      </div>
+      <div className="navbar-external flex flex-col justify-start align-top">
+        <li className="navbar-email text-almostBlack font-forma relative flex items-center font-[300] text-[3vh] leading-none hover:text-blue transition duration-500 ease-in-out">
+          <a href="mailto:neelk2@illinois.edu" target="_blank" rel="noreferrer">
+            <NavbarItem text={"email"} />
+          </a>
+        </li>
+        <li className="navbar-github text-almostBlack font-forma relative flex items-center font-[300] text-[3vh] leading-none hover:text-githubGrey transition duration-500 ease-in-out">
+          <a href="https://github.com/Szplugz" target="_blank" rel="noreferrer">
+            <NavbarItem text={"github"} />
+          </a>
+        </li>
+        <li className="navbar-instagram text-almostBlack font-forma relative flex items-center font-[300] text-[3vh] leading-none hover:text-instagramRed transition duration-500 ease-in-out">
+          <a
+            href="https://www.instagram.com/secondhandneel/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <NavbarItem text={"instagram"} />
+          </a>
+        </li>
+        <li className="navbar-twitter text-almostBlack font-forma relative flex items-center font-[300] text-[3vh] leading-none hover:text-twitterBlue transition duration-500 ease-in-out">
+          <a
+            href="https://www.instagram.com/secondhandneel/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <NavbarItem text={"twitter"} />
+          </a>
+        </li>
+        <li className="navbar-youtube text-almostBlack font-forma relative flex items-center font-[300] text-[3vh] leading-none hover:text-youtubeRed transition duration-500 ease-in-out">
+          <a
+            href="https://www.instagram.com/secondhandneel/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <NavbarItem text={"youtube"} />
+          </a>
+        </li>
+      </div>
+      <div className="navbar-mobile">
+        {toggleMenu ? (
+          <img
+            className="x-button h-[6vh] w-auto"
+            src={x}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <img
+            className="plus-button h-[6vh] w-auto"
+            src={plus}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="navbar-menu-mobile flex flex-col overflow-y-auto">
+            <li className="navbar-neel text-lightpink font-forma relative flex items-center font-[300] text-[3vh] hover:text-eggshell transition duration-500 ease-in-out leading-none">
+              Neel Khare
+            </li>
+            <li className="navbar-bits text-eggshell font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+              <NavbarItem text={"Bits"} />
+            </li>
+            <li className="navbar-intrigue text-eggshell font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+              <NavbarItem text={"Intrigue"} />
+            </li>
+            <li className="navbar-lyrics text-eggshell font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+              <NavbarItem text={"Lyrics"} />
+            </li>
+            <li className="navbar-people text-eggshell font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+              <NavbarItem text={"People"} />
+            </li>
+            <li className="navbar-photography text-eggshell font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+              <NavbarItem text={"Photography"} />
+            </li>
+            <li className="navbar-projects text-eggshell font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+              <NavbarItem text={"Projects"} />
+            </li>
+            <li className="navbar-quotes text-eggshell font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+              <NavbarItem text={"Quotes"} />
+            </li>
+            <li className="navbar-item">
+              <a
+                href="https://drive.google.com/file/d/1Opg8P9ej8cd1tPoKEsSkcYo7F4J3Uz63/view?usp=sharing"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <NavbarItem text={"Resumé"} />
+              </a>
+            </li>
+            <li className="navbar-websites text-eggshell font-forma relative flex items-center font-[300] text-[3vh] leading-none">
+              <NavbarItem text={"Websites"} />
+            </li>
+          </div>
+        )}
+      </div>
+      <img className="picture rounded w-[100%] mb-[1vh]" src={fallpic}></img>
+    </div>
+  );
+};
+
+const NavbarItem = ({ text }) => <div>{text}</div>;
+
+export default Navbar;
---
+++ src/pages/about/About.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+import biopic from "../../utils/biopic.jpg";
+import { Helmet } from "react-helmet";
+import "./About.css";
+
+const About = () => {
+  return (
+    <div className="about-page bg-eggshell flex flex-row justify-center selection:bg-skyBlue">
+      <Helmet>
+        <title>About | Neel Khare</title>
+      </Helmet>
+      <Navbar />
+      <div className="about-bio float flex-col w-[30%] my-[10vh] ml-[7.5vw] overflow-y-auto">
+        <li className="navbar-neel text-lightpink relative flex items-center font-[400] font-inter mb-[10vh]">
+          Neel Khare
+        </li>
+        <div className="about-bio-content flex-col w-full text-almostBlack font-inter font-[400] leading-snug overflow-y-auto">
+          <p className="p1 mb-[10vh] text-[2.25vh] font-[500]">
+            Hello! I'm a junior at the University of Illinois, Urbana-Champaign,
+            where I spend most of the year studying computer science and writing
+            code. What matters to me the most is consistent personal growth and
+            sharing my knowledge with the world in the hope of lifting others
+            up. At the moment, I'm quite interested in full-stack development
+            and product design, and I am always looking to involve myself in
+            projects that improve people’s quality of life. I find myself using
+            C++ and Javascript the most (so you can be assured that I have a
+            high pain threshold).
+          </p>
+          <h1 className="h1 mb-[3vh] text-lightpink font-[400]">
+            What keeps me busy these days:
+          </h1>
+          <div>
+            <li className="bullet ml-[4vh] mb-[1vh]">
+              <span className="p1">
+                At college, I am a software developer at{" "}
+                <a
+                  className="link text-almostBlack underline hover:decoration-solid decoration-dotted"
+                  href="https://www.quantillinois.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Quant
+                </a>
+                , and co-founder and President at August Productions, where we
+                make films and documentaries.
+              </span>
+            </li>
+            <li className="bullet ml-[4vh] mb-[1vh]">
+              <span className="p2">
+                I build and maintain side projects that mostly consist of
+                websites and handy web-applications.
+              </span>
+            </li>
+            <li className="bullet ml-[4vh] mb-[3vh]">
+              <span className="p2">
+                I'm a full-stack engineering intern at{" "}
+                <a
+                  className="link text-lightpink"
+                  href="https://www.mediaocean.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Mediaocean
+                </a>
+                .
+              </span>
+            </li>
+            <p className="p1 mb-[3vh]">
+              {" "}
+              In my free time, I like roaming around Chicago, exploring cafés,
+              watching trains, taking pictures, and reading. I love learning
+              about human behavior, transportation, and startups, among many
+              other things.
+            </p>
+            <p className="p1 mb-[3vh]">
+              I love connecting with and meeting new people, so please feel free
+              to reach out to me through any of the social links that you see on
+              the left! I would <em>especially</em> love to hear from you if:
+              <div className="list mt-[3vh]">
+                <p className="about-cat-1 ml-[5%] mb-[1vh]">
+                  {" "}
+                  <span className="1 text-lightpink font-tablet">1.&nbsp;</span>
+                  You are a recruiter looking for a software engineering intern.
+                </p>
+                <p className="about-cat-2 ml-[5%] mb-[1vh]">
+                  {" "}
+                  <span className="2 text-lightpink font-tablet">2.&nbsp;</span>
+                  You are a developer/filmmaker/photographer looking to
+                  collaborate on a project.
+                </p>
+                <p className="about-cat-3 ml-[5%] mb-[1vh]">
+                  {" "}
+                  <span className="3 text-lightpink font-tablet">3.&nbsp;</span>
+                  If you would like to talk to (or argue with) me about one of
+                  my many{" "}
+                  <span className="about-interests-link text-lightpink font-tablet">
+                    interests
+                  </span>{" "}
+                  .
+                </p>
+              </div>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
