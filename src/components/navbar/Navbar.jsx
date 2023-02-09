cad195f8158c9434705a5a7728ba8bda0126c068
Neel <neelk2@illinois.edu>
2023-02-08 19:30:54 -0600

    Change home screen layout + some sexy animations

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,62 +1,40 @@
 import React from "react";
+import NavbarItem from "./NavbarItem";
 
 const Navbar = () => {
   return (
-    <div className="navbar-box flex flex-col fixed left-[15vw] h-[85vh] align-top w-[15%] min-w-480 my-[10vh] mr-[7.5vw]">
+    <div className="navbar-box flex flex-col fixed left-[20vw] h-[85vh] align-top w-[15%] min-w-480 my-[10vh] mr-[7.5vw]">
       <div className="navbar-menu flex flex-col mb-[7vh]">
-        <li className="navbar-item">
+        <div className="navbar-item">
           <a href="/about">
-            <NavbarItem text={"Home"} />
+            <NavbarItem text={"home"} />
           </a>
-        </li>
-        <li className="navbar-index text-gray-600 font-forma relative flex items-center font-[300] text-2xl leading-tight">
-          <NavbarItem text={"Index"} />
-        </li>
-        <li className="navbar-item">
-          <a href="/people">
-            <NavbarItem text={"People"} />
-          </a>
-        </li>
-        <li className="navbar-item">
-          <a href="/photography">
-            <NavbarItem text={"Photography"} />
-          </a>
-        </li>
-        <li className="navbar-item">
-          <a href="https://read.cv/szplugz" target="_blank" rel="noreferrer">
-            <NavbarItem text={"Resumé"} />
-          </a>
-        </li>
-        <li className="navbar-bits text-gray-600 font-forma relative flex items-center font-[300] text-2xl leading-tight">
-          <NavbarItem text={"Tapas"} />
-        </li>
-        <li className="navbar-item">
-          <a href="/work">
-            <NavbarItem text={"Work"} />
-          </a>
-        </li>
+        </div>
+        <div className="navbar-item">
+          <NavbarItem text={"index"} invalid={true} />
+        </div>
       </div>
       <div className="navbar-external flex flex-col justify-start align-top">
-        <li className="navbar-arena text-almostBlack font-forma relative flex items-center font-[300] text-2xl leading-tight hover:text-slate transition duration-500 ease-in-out">
+        <div className="navbar-item">
           <a
             href="https://www.are.na/neel-khare"
             target="_blank"
             rel="noreferrer"
           >
-            <NavbarItem text={"Are.na"} />
+            <NavbarItem text={"are.na"} />
           </a>
-        </li>
-        <li className="navbar-email text-almostBlack font-forma relative flex items-center font-[300] text-2xl leading-tight hover:text-blue transition duration-500 ease-in-out">
+        </div>
+        <div className="navbar-item">
           <a href="mailto:neelk2@illinois.edu" target="_blank" rel="noreferrer">
             <NavbarItem text={"email"} />
           </a>
-        </li>
-        <li className="navbar-github text-almostBlack font-forma relative flex items-center font-[300] text-2xl leading-tight hover:text-githubGrey transition duration-500 ease-in-out">
+        </div>
+        <div className="navbar-item">
           <a href="https://github.com/Szplugz" target="_blank" rel="noreferrer">
             <NavbarItem text={"github"} />
           </a>
-        </li>
-        <li className="navbar-twitter text-almostBlack font-forma relative flex items-center font-[300] text-2xl leading-tight hover:text-twitterBlue transition duration-500 ease-in-out">
+        </div>
+        <div className="navbar-item">
           <a
             href="https://www.twitter.com/szplugz/"
             target="_blank"
@@ -64,8 +42,8 @@ const Navbar = () => {
           >
             <NavbarItem text={"twitter"} />
           </a>
-        </li>
-        <li className="navbar-youtube text-almostBlack font-forma relative flex items-center font-[300] text-2xl leading-tight hover:text-youtubeRed transition duration-500 ease-in-out">
+        </div>
+        <div className="navbar-item">
           <a
             href="https://www.youtube.com/kiltoch/"
             target="_blank"
@@ -73,12 +51,10 @@ const Navbar = () => {
           >
             <NavbarItem text={"youtube"} />
           </a>
-        </li>
+        </div>
       </div>
     </div>
   );
 };
-
-const NavbarItem = ({ text }) => <div>{text}</div>;
 
 export default Navbar;
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HomeNav from "../../components/navbar/HomeNav";
 import Navmobile from "../../components/navbar/Navmobile";
+import Navbar from "../../components/navbar/Navbar";
 import { Helmet } from "react-helmet";
 import fallpic from "../../utils/fallrl2square.jpg";
 import "./About.css";
@@ -9,102 +10,118 @@ import headshot from "../../utils/chiheadshot.jpg";
 
 const About = () => {
   return (
-    <div className="about-page bg-eggshell2 h-screen flex flex-row justify-center selection:bg-skyBlue">
+    <div className="about-page bg-eggshell2 h-screen flex flex-row justify-center selection:bg-skyBlue break-normal">
       <Helmet>
         <title>About | Neel Khare</title>
       </Helmet>
-      <HomeNav />
-      <div className="about-bio float flex-col w-[40%] my-[10vh] ml-[15vw] overflow-y-auto">
-        <Navmobile />
-        <div className="desktop-head flex justify-between">
-          <div className="navbar-neel text-navy relative flex items-center text-3xl font-[500] font-inter mb-[5vh]">
-            Hello, I'm Neel.
-          </div>
-          <img className="mb-[5vh] h-[2rem]" src={newlogo}></img>
+      <div className="about-content bg-eggshell2 w-[90%] h-screen flex flex-row justify-center">
+        <div>
+          <Navbar />
         </div>
-        <div className="about-bio-content overflow-x-hidden flex-col w-full text-almostBlack font-inter font-[400] text-base sm:text-base leading-snug overflow-y-auto">
-          <p className="about-bio-main mb-5">
-            I study computer science, economics, and psychology at the{" "}
-            <span className="text-[#ff6541]">
-              University of Illinois, Urbana-Champaign
-            </span>
-            . Currently, I'm curious about high-frequency trading and
-            computational neuroscience. Outside of class, I spend most of my
-            time taking{" "}
-            <a
-              className="link text-almostBlack underline hover:decoration-solid decoration-dotted decoration-chinaRose transition duration-[1000ms] ease-in-out"
-              href="/photography"
-              target="_blank"
-              rel="noreferrer"
-            >
-              pictures
-            </a>
-            , building websites, exercising, and socializing.
-          </p>
-          <div className="technology-header flex flex-row justify-between items-center text-chinaRose mb-3 text-xl font-[500]">
-            Presently
+        <div className="about-bio float flex-col w-[40%] ml-20 mt-[10vh] overflow-y-auto">
+          <Navmobile />
+          <div className="desktop-head flex justify-between">
+            <div className="navbar-neel text-navy relative flex items-center text-3xl font-[500] font-inter mb-[5vh]">
+              Hello, I'm Neel.
+            </div>
+            <img className="mb-[5vh] h-[2rem]" src={newlogo}></img>
           </div>
-          <ul className="list-disc pl-5 mb-5 marker:text-chinaRose">
-            <li className="list-item">
-              Building an order-matching engine at{" "}
+          <div className="about-bio-content overflow-x-hidden flex-col w-full text-almostBlack font-inter font-[400] text-base sm:text-base leading-snug overflow-y-auto">
+            <p className="about-bio-main mb-5">
+              I study computer science, economics, and psychology at the{" "}
+              <span className="text-[#ff6541]">
+                University of Illinois, Urbana-Champaign
+              </span>
+              . Currently, I'm curious about high-frequency trading and
+              computational neuroscience. Outside of class, I spend most of my
+              time taking{" "}
               <a
                 className="link text-almostBlack underline hover:decoration-solid decoration-dotted decoration-chinaRose transition duration-[1000ms] ease-in-out"
-                href="https://uiucquant.web.illinois.edu/"
+                href="/photography"
                 target="_blank"
                 rel="noreferrer"
               >
-                Quant
+                pictures
               </a>
-              .
-            </li>
-            <li>
-              Developing a website and producing video content for{" "}
+              , building websites, exercising, and socializing.
+            </p>
+            <div className="technology-header flex flex-row justify-between items-center text-chinaRose mb-3 text-xl font-[500]">
+              Presently
+            </div>
+            <ul className="list-disc pl-5 mb-5 marker:text-chinaRose">
+              <li className="list-item">
+                Building an order-matching engine at{" "}
+                <a
+                  className="link text-almostBlack underline hover:decoration-solid decoration-dotted decoration-chinaRose transition duration-[1000ms] ease-in-out"
+                  href="https://uiucquant.web.illinois.edu/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Quant
+                </a>
+                .
+              </li>
+              <li>
+                Developing a website and producing video content for{" "}
+                <a
+                  className="link text-almostBlack underline hover:decoration-solid decoration-dotted decoration-chinaRose transition duration-[1000ms] ease-in-out"
+                  href="https://www.instagram.com/tfnuiuc/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  The Fashion Network
+                </a>
+                .
+              </li>
+              <li>
+                Making short films and documentaries at August Productions.
+              </li>
+            </ul>
+            <div className="technology-header flex flex-row justify-between items-center text-chinaRose mb-3 text-xl font-[500]">
+              In the past
+            </div>
+            <ul className="list-disc pl-5 mb-5 marker:text-chinaRose">
+              <li className="list-item">
+                Designed and developed the frontend architecture for Quant.
+              </li>
+              <li>
+                Taught students the fundamentals of Object-Oriented Programming
+                as a course assistant for{" "}
+                <a
+                  className="link text-almostBlack underline hover:decoration-solid decoration-dotted decoration-chinaRose transition duration-[1000ms] ease-in-out"
+                  href="https://www.cs124.org/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  CS 124
+                </a>{" "}
+              </li>
+            </ul>
+            <p className="about-bio-main mb-5">
+              I'm always open to making new friends, learning new things,
+              working on cool projects, or just chatting! You can reach me at{" "}
+              <br></br>
               <a
                 className="link text-almostBlack underline hover:decoration-solid decoration-dotted decoration-chinaRose transition duration-[1000ms] ease-in-out"
-                href="https://www.instagram.com/tfnuiuc/"
+                href="mailto:neelk2"
                 target="_blank"
                 rel="noreferrer"
               >
-                The Fashion Network
-              </a>
-              .
-            </li>
-            <li>Making short films and documentaries at August Productions.</li>
-          </ul>
-          <div className="technology-header flex flex-row justify-between items-center text-chinaRose mb-3 text-xl font-[500]">
-            In the past
-          </div>
-          <ul className="list-disc pl-5 mb-5 marker:text-chinaRose">
-            <li className="list-item">
-              Designed and developed the frontend architecture for Quant.
-            </li>
-            <li>
-              Taught students the fundamentals of Object-Oriented Programming as
-              a course assistant for{" "}
-              <a
-                className="link text-almostBlack underline hover:decoration-solid decoration-dotted decoration-chinaRose transition duration-[1000ms] ease-in-out"
-                href="https://www.cs124.org/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                CS 124
+                neelk2<span className="text-chinaRose">&#123;</span>at
+                <span className="text-chinaRose">&#125;</span>illinois.edu
               </a>{" "}
-            </li>
-          </ul>
-          <p className="about-bio-main mb-5">
-            I'm always open to making new friends, learning new things, working
-            on cool projects, or just chatting! You can reach me at any of the
-            social links you see on the side.
-          </p>
-          <hr className="line-2 border-dotted mb-[8vh] h-[1px] border-navy"></hr>
-          <div className="quote text-slate-600 font-inter text-sm mb-[4.25rem] flex justify-end">
-            <em>After a while, determination starts to look like talent.</em>
+              or at any of the social links you see on the side.
+            </p>
+            <hr className="line-2 border-dotted mb-[8vh] h-[1px] border-navy"></hr>
+            <div className="quote text-slate-600 font-inter text-sm mb-[4.25rem] flex justify-end">
+              <em>After a while, determination starts to look like talent.</em>
+            </div>
           </div>
+          <img
+            className="picture-mobile rounded w-[100%] mb-[1vh]"
+            src={headshot}
+          ></img>
         </div>
-        <img
-          className="picture-mobile rounded w-[100%] mb-[1vh]"
-          src={headshot}
-        ></img>
       </div>
     </div>
   );
