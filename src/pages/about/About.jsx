2ad4ba24e4c8b3a6e5acd671c031b093feea0413
Neel <neelk2@illinois.edu>
2023-02-10 13:22:02 -0600

    Fixed scroll for desktop

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -11,7 +11,7 @@ import MobileFooter from "../../components/navbar/MobileFooter";
 
 const About = () => {
   return (
-    <div className="about-page bg-eggshell2 h-screen flex flex-row justify-center selection:bg-skyBlue break-normal">
+    <div className="about-page bg-eggshell2 h-screen flex flex-row justify-center selection:bg-skyBlue break-normal overflow-y-scroll">
       <Helmet>
         <title>About | Neel Khare</title>
       </Helmet>
@@ -19,7 +19,7 @@ const About = () => {
         <div>
           <Navbar />
         </div>
-        <div className="about-bio float flex-col w-[40%] ml-20 mt-[10vh] overflow-y-scroll">
+        <div className="about-bio float flex-col w-[40%] ml-20 mt-[10vh] overflow-y-visible">
           <Navmobile />
           <div className="quote-mobile text-slate-600 font-inter text-sm mb-5 flex justify-end">
             <em>After a while, determination starts to look like talent.</em>
@@ -30,7 +30,7 @@ const About = () => {
             </div>
             <img className="mb-5 sm:h-[2rem] h-[1.75rem]" src={newlogo}></img>
           </div>
-          <div className="about-bio-content overflow-x-hidden flex-col w-full text-almostBlack font-inter font-[400] text-base sm:text-base leading-snug overflow-y-scroll">
+          <div className="about-bio-content overflow-x-hidden flex-col w-full text-almostBlack font-inter font-[400] text-base sm:text-base leading-snug">
             <p className="about-bio-main mb-5">
               I study computer science, economics, and psychology at the{" "}
               <span className="text-[#ff6541]">
