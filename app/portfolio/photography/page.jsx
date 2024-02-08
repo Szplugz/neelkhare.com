1e7a7fc0dd96473d6429dd53be9ea2b653e1909c
Neel <neelk2@illinois.edu>
2024-02-07 18:22:16 -0600

    elementary sorting algorithm for pictures

--- app/portfolio/photography/page.jsx
+++ app/portfolio/photography/page.jsx
@@ -8,17 +8,62 @@ const Photography = async ({ searchParams }) => {
   const currentCity = searchParams?.city || "copenhagen";
   // See if there's a way to update the url to reflect copenhagen without turning this into a client component
 
-  const images = await fetchImages(currentCity);
-  const leftImages = images.filter(({ public_id }) => {
-    return parseInt(public_id[public_id.length - 1]) % 2 == 0;
-  });
-  const rightImages = images.filter(({ public_id }) => {
-    return parseInt(public_id[public_id.length - 1]) % 2 == 1;
-  });
+  const { wideImages, tallImages } = await fetchImages(currentCity);
+  const leftImages = [];
+  const rightImages = [];
+  let left = true;
+  while (wideImages.length && tallImages.length) {
+    const wideImage = wideImages.shift();
+    const tallImage = tallImages.shift();
+    if (left) {
+      leftImages.push(wideImage);
+      leftImages.push(tallImage);
+    } else {
+      rightImages.push(tallImage);
+      rightImages.push(wideImage);
+    }
+    left = !left;
+  }
+  // only one of these will be true
+  while (wideImages.length) {
+    // imageRows.push([wideImages.shift(), wideImages.shift()]);
+    left
+      ? leftImages.push(wideImages.shift())
+      : rightImages.push(wideImages.shift());
+    left = !left;
+  }
+  while (tallImages.length) {
+    // imageRows.push([wideImages.shift(), wideImages.shift()]);
+    left
+      ? leftImages.push(tallImages.shift())
+      : rightImages.push(tallImages.shift());
+    left = !left;
+  }
+
   return (
     <div className="photography-contents">
       <PhotoMenuBar />
       <div className="photo-gallery">
+        {/* {imageRows.map((row) => {
+          return (
+            <div className="image-row flex flex-row">
+              <Image
+                src={`https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${row[0].public_id}.${row[0].format}`}
+                width={720}
+                height={480}
+                key={row[0].id}
+                alt="Next.js Conf photo"
+              />
+              <Image
+                src={`https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${row[1].public_id}.${row[1].format}`}
+                width={720}
+                height={480}
+                key={row[1].id}
+                alt="Next.js Conf photo"
+              />
+            </div>
+          );
+        })} */}
         <div className="photo-gallery-left">
           {leftImages.map(({ id, public_id, format }) => {
             return (
@@ -51,24 +96,31 @@ const Photography = async ({ searchParams }) => {
 };
 
 const fetchImages = async (city) => {
-  console.log(city);
   const results = await cloudinary.search
     .expression(`folder:${process.env.CLOUDINARY_FOLDER}/${city}/*`)
     .sort_by("public_id", "asc")
     .max_results(400)
     .execute();
 
-  let reducedResults = [];
+  let reducedResults = { wideImages: [], tallImages: [] };
 
   let i = 0;
   for (let result of results.resources) {
-    reducedResults.push({
-      id: i,
-      height: result.height,
-      width: result.width,
-      public_id: result.public_id,
-      format: result.format,
-    });
+    result.width >= result.height
+      ? reducedResults.wideImages.push({
+          id: i,
+          height: result.height,
+          width: result.width,
+          public_id: result.public_id,
+          format: result.format,
+        })
+      : reducedResults.tallImages.push({
+          id: i,
+          height: result.height,
+          width: result.width,
+          public_id: result.public_id,
+          format: result.format,
+        });
     i++;
   }
 
